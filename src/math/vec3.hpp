0af3c7ade5ad95d36bb96812dda8d12b94f82252
Adam <safety5646@gmail.com>
2026-05-22 00:21:18 -0500

    Reenabled implicit scalar conversion for construction. Prevented implicit conversion between decimal and non-decimal types. Implemented basic math functions in namespace math.

--- src/math/vec3.hpp
+++ src/math/vec3.hpp
@@ -2,11 +2,14 @@
 #define VEC3_HPP
 #include <cmath>
 #include <type_traits>
-template <typename T>
+#include <concepts>
+#include <iostream>
+
 
 /*
 NOTE: SCALAR AND STANDARD OPERATORS ARE SEPARATE FOR PERFORMANCE
 */
+template <typename T>
 struct vec3{
     T x, y, z;
     // CONSTRUCTORS
@@ -16,15 +19,20 @@ struct vec3{
     vec3(T _x, T _y, T _z): x(_x), y(_y), z(_z){}
     
     // for explicit scalar construction
-    explicit vec3(T val): x(val), y(val), z(val) {}
+    vec3(T val): x(val), y(val), z(val) {}
 
-    // for casting
-    template<typename U>
-    vec3(const vec3<U>& other):
-        x(other.x), y(other.y), z(other.z) {}
+    
+    // for explicit casting of float to non float conversion, and implicit casting within the two domains.
 
-    // ARITHMETIC OPERATIONS
-
+    template <typename U>
+    requires std::is_constructible_v<T, U>
+    explicit(
+        ((std::is_floating_point_v<U> && !std::is_floating_point_v<T>) ||
+        (!std::is_floating_point_v<U> && std::is_floating_point_v<T>))
+    )
+    vec3(const vec3<U>& other)
+        : x(other.x), y(other.y), z(other.z)
+    {}
     // ADDITION
     vec3<T> operator+(const vec3<T>& other) const{
         return vec3<T>(x + other.x, y + other.y, z + other.z);
@@ -107,25 +115,100 @@ struct vec3{
         z *= fact;
         return *this;
     }
+
     
     // LOGIC OPERATORS 
 
     // default member comparison methods inlcuded
     auto operator<=>(const vec3&) const = default;
+
+
+    bool allLessThan(const T val) const {
+        return x < val && y < val && z < val;
+    }
+    bool allGreaterThan(const T val) const {
+        return x > val && y > val && z > val;
+    }
+
+
     
 };
-// FREE FUNCTIONS
+namespace math{
+    
+    // STATELESS FUNCTIONS
 
-// ROUNDING
 
-template<typename T>
-requires std::is_floating_point_v<T>
-vec3<T> floor(const vec3<T>& v) {
-    return {
-        static_cast<T>(std::floor(v.x)),
-        static_cast<T>(std::floor(v.y)),
-        static_cast<T>(std::floor(v.z))
-    };
+    // NUMERICAL HELPERS: 
+    
+    template<typename Float_T>
+    concept Floating = std::is_floating_point_v<Float_T>;
+
+    template<typename Signed_Numeric_T>
+    concept Signed_Numeric = std::is_arithmetic_v<Signed_Numeric_T> && std::is_signed_v<Signed_Numeric_T>;
+
+    //ROUNDING
+    template<Floating T>
+    vec3<T> floor(const vec3<T>& v) {
+        return {
+            static_cast<T>(std::floor(v.x)),
+            static_cast<T>(std::floor(v.y)),
+            static_cast<T>(std::floor(v.z))
+        };
+    }
+    template<Floating T>
+    vec3<T> ceil(const vec3<T>& v) {
+        return {
+            static_cast<T>(std::ceil(v.x)),
+            static_cast<T>(std::ceil(v.y)),
+            static_cast<T>(std::ceil(v.z))
+        };
+    }
+    template<Floating T>
+    vec3<T> round(const vec3<T>& v) {
+        return {
+            static_cast<T>(std::round(v.x)),
+            static_cast<T>(std::round(v.y)),
+            static_cast<T>(std::round(v.z))
+        };
+    }
+    template<Signed_Numeric T>
+    vec3<T> abs(const vec3<T>& v){
+        return {
+            static_cast<T>(std::abs(v.x)),
+            static_cast<T>(std::abs(v.y)),
+            static_cast<T>(std::abs(v.z))
+        };
+    }
+
+    template<Signed_Numeric T>
+    vec3<T> trunc(const vec3<T>& v){
+        return {
+            static_cast<T>(std::trunc(v.x)),
+            static_cast<T>(std::trunc(v.y)),
+            static_cast<T>(std::abs(v.z))
+        };
+    }
+
+    // STABILITY HELPERS
+    template<Floating T>
+    bool roughEquals(vec3<T> a, vec3<T> b, T eps = 1e-7){
+        return math::abs(a - b).allLessThan(eps);
+    }
+
+    template<Floating T>
+    bool is_zero(vec3<T> v, T eps = 1e-7){
+        vec3<T> zeroV;
+
+        return roughEquals(v, zeroV, eps);
+    }
+
+    // LOGGING
+    template<typename T>
+    std::ostream& operator<<(std::ostream& os, const vec3<T>& v){
+        return os << "{" << v.x << ", " << v.y << ", " << v.z;
+    }
+
+
 }
 
 
